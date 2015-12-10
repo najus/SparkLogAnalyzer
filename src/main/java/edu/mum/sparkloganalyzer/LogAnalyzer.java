6a91f92584afdabec23efb20098ff7c4dc4ce04c
Sujan <najussujan@gmail.com>
2015-12-10 12:16:12 -0600

    ApacheAccessLog POJO, log.txt added

--- src/main/java/edu/mum/sparkloganalyzer/LogAnalyzer.java
+++ src/main/java/edu/mum/sparkloganalyzer/LogAnalyzer.java
@@ -1,8 +1,15 @@
 package edu.mum.sparkloganalyzer;
 
+import java.io.Serializable;
+import java.util.Comparator;
+import java.util.List;
+
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function2;
+
+import scala.Tuple2;
 
 /**
  * 
@@ -10,8 +17,23 @@ import org.apache.spark.api.java.JavaSparkContext;
  *
  */
 public class LogAnalyzer {
+	private static Function2<Long, Long, Long> SUM_REDUCER = (a, b) -> a + b;
+
+	private static class ValueComparator<K, V> implements Comparator<Tuple2<K, V>>, Serializable {
+		private Comparator<V> comparator;
+
+		public ValueComparator(Comparator<V> comparator) {
+			this.comparator = comparator;
+		}
+
+		@Override
+		public int compare(Tuple2<K, V> o1, Tuple2<K, V> o2) {
+			return comparator.compare(o1._2(), o2._2());
+		}
+	}
+
 	public static void main(String[] args) {
-		
+
 		// Create a Spark Context.
 		SparkConf conf = new SparkConf().setAppName("Log Analyzer");
 		JavaSparkContext sc = new JavaSparkContext(conf);
@@ -25,6 +47,34 @@ public class LogAnalyzer {
 		JavaRDD<String> logLines = sc.textFile(logFile);
 
 		// TODO: Insert code here for processing logs.
+
+		// Convert the text log lines to ApacheAccessLog objects and cache them
+		// since multiple transformations and actions will be called on that
+		// data.
+		JavaRDD<ApacheAccessLog> accessLogs = logLines.map(ApacheAccessLog::parseFromLogLine).cache();
+
+		// Calculate statistics based on the content size.
+		// Note how the contentSizes are cached as well since multiple actions
+		// are called on that RDD.
+		JavaRDD<Long> contentSizes = accessLogs.map(ApacheAccessLog::getContentSize).cache();
+		System.out.println(String.format("Content Size Avg: %s, Min: %s, Max: %s",
+				contentSizes.reduce(SUM_REDUCER) / contentSizes.count(), contentSizes.min(Comparator.naturalOrder()),
+				contentSizes.max(Comparator.naturalOrder())));
+
+		// Compute Response Code to Count.
+		List<Tuple2<Integer, Long>> responseCodeToCount = accessLogs
+				.mapToPair(log -> new Tuple2<>(log.getResponseCode(), 1L)).reduceByKey(SUM_REDUCER).take(100);
+		System.out.println(String.format("Response code counts: %s", responseCodeToCount));
+
+		// Any IPAddress that has accessed the server more than 10 times.
+		List<String> ipAddresses = accessLogs.mapToPair(log -> new Tuple2<>(log.getIpAddress(), 1L))
+				.reduceByKey(SUM_REDUCER).filter(tuple -> tuple._2() > 10).map(Tuple2::_1).take(100);
+		System.out.println(String.format("IPAddresses > 10 times: %s", ipAddresses));
+
+		// Top Endpoints.
+		List<Tuple2<String, Long>> topEndpoints = accessLogs.mapToPair(log -> new Tuple2<>(log.getEndpoint(), 1L))
+				.reduceByKey(SUM_REDUCER).top(10, new ValueComparator<>(Comparator.<Long> naturalOrder()));
+		System.out.println(String.format("Top Endpoints: %s", topEndpoints));
 
 		sc.stop();
 	}
